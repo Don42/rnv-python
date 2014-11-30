6d4e0e14bdddbffff94ab7b8ceded3fde870dc1f
Marco don Kaulea <DonMarco42@gmail.com>
2014-11-30 21:27:04 +0100

    Add API documentation
    Closes #4

--- rnv_api.py
+++ rnv_api.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
 # "THE SCOTCH-WARE LICENSE" (Revision 42):
 # <DonMarco42@gmail.com> wrote this file. As long as you retain this notice you
@@ -5,7 +6,23 @@
 # this stuff is worth it, you can buy me a scotch whisky in return
 # Marco 'don' Kaulea
 # ----------------------------------------------------------------------------
+"""Bindings for the Web-API from rnv.
 
+Attributes:
+    USER_AGENT (string): User agent sent to the server.
+    API_DOMAIN (string): Protocol, host, and port of the URL.
+    SITE_URL (string): Part of the URL that stays the same for all requests.
+    DEPARTURES_URL (string): Part of the URL to query the departures.
+    STATIONS_URL (string): Part of the URL to query the stations.
+    NEWS_URL (string): Part of the URL to query news.
+    NEWS_COUNT_URL (string): Part of the URL to query the news count.
+    TICKER_URL (string): Part of the URL to query tickers.
+    TICKER_COUNT_URL (string): Part of the URL to query ticker count
+        to prettyfied json.
+    stations (list): Cache of all stations.
+        It is filled and updated by get_stations().
+
+"""
 import requests
 import json
 import re
@@ -28,6 +45,21 @@ stations = None
 
 
 def find_station_by_short_name(name):
+    """Find a station by its short name
+
+    Matches are case insensitiv.
+    Only the first of several matches is returned.
+    Args:
+        name (string): short name of the station.
+        The string has to be a valid short name.
+
+    Returns:
+        dict: Station object  if found
+
+    Raises:
+        StopIteration: If no station is found
+
+    """
     if stations is None:
         get_stations()
     name = name.upper()
@@ -36,6 +68,20 @@ def find_station_by_short_name(name):
 
 
 def find_station_by_long_name(name):
+    """Find a station by its long name
+
+    Matches are case insensitiv.
+    Only the first of several matches is returned.
+    Args:
+        name (string): long name of the station.
+
+    Returns:
+        dict: Station object  if found
+
+    Raises:
+        StopIteration: If no station is found
+
+    """
     if stations is None:
         get_stations()
     name = name.casefold()
@@ -44,6 +90,16 @@ def find_station_by_long_name(name):
 
 
 def find_stations_by_long_name(name):
+    """Find stations by their long name
+
+    Matches are case insensitiv.
+    Args:
+        name (string): long name of the station.
+
+    Returns:
+        generator: This generator allows to iterate over all matches.
+
+    """
     if stations is None:
         get_stations()
     name = name.casefold()
@@ -51,6 +107,18 @@ def find_stations_by_long_name(name):
 
 
 def get_hafasid_from_name(name):
+    """Find the hafas id by searching the stations vor a provied name
+
+    Args:
+        name (string): Either short or long name
+
+    Returns:
+        string: hafas id
+
+    Raises:
+        StopIteration: If no station is found
+
+    """
     hafasid = None
     if len(name) == 4 and name.isupper():
         return find_station_by_short_name(name)['hafasID']
@@ -63,6 +131,22 @@ def get_departures(hafasID,
                    transport_filter=None,
                    time=None,
                    ui_source='LINE'):
+    """Get departures by hafas id
+
+    Args:
+        hafasID (int): This can be looked up by get_hafasid_from_name.
+        transport_filter (string, optional): Filter departure by line.
+            Defaults to None.
+        time (string): Query departures from specific time. This has
+            to be a complete ISO-Date "YYYY-MM-DD HH:MM"
+
+    Returns:
+        list: List of departure dictionaries
+
+    Raises:
+        Exception: If there are no departures
+
+    """
     payload = {'hafasID': hafasID,
                'transportFilter': transport_filter,
                'time': time if time is not None else 'null',
@@ -85,6 +169,12 @@ def get_departures(hafasID,
 
 
 def get_stations():
+    """Retrieves stations and store into cache
+
+    Returns:
+        list: List of stations dictionaries
+
+    """
     data = json.loads(get_stations_raw())
     global stations
     stations = data['stations']
@@ -92,6 +182,15 @@ def get_stations():
 
 
 def get_stations_raw():
+    """Retrieves stations
+
+    Returns:
+        string: Unparsed content from the API
+
+    Raises:
+        Exception: If the status code is not 200
+
+    """
     headers = {'Accept': 'application/json',
                'Accept-Language': 'de',
                'User-Agent': USER_AGENT}
@@ -105,6 +204,15 @@ def get_stations_raw():
 
 
 def get_news():
+    """Retrieves news
+
+    Returns:
+        list: List of news dictionaries
+
+    Raises:
+        Exception: If the status code is not 200
+
+    """
     headers = {'Accept': 'application/json',
                'Accept-Language': 'de',
                'User-Agent': USER_AGENT}
@@ -118,6 +226,15 @@ def get_news():
 
 
 def get_news_count():
+    """Retrieves number of news items.
+
+    Returns:
+        int: Number of news items
+
+    Raises:
+        Exception: If the status code is not 200
+
+    """
     headers = {'Accept': 'application/json',
                'Accept-Language': 'de',
                'User-Agent': USER_AGENT}
@@ -133,6 +250,15 @@ def get_news_count():
 
 
 def get_ticker():
+    """Retrieves ticker.
+
+    Returns:
+        list: List of ticker dictionaries
+
+    Raises:
+        Exception: If the status code is not 200
+
+    """
     headers = {'Accept': 'application/json',
                'Accept-Language': 'de',
                'User-Agent': USER_AGENT}
@@ -146,6 +272,15 @@ def get_ticker():
 
 
 def get_ticker_count():
+    """Retrieves number of ticker items.
+
+    Returns:
+        int: Number of ticker items
+
+    Raises:
+        Exception: If the status code is not 200
+
+    """
     headers = {'Accept': 'application/json',
                'Accept-Language': 'de',
                'User-Agent': USER_AGENT}
